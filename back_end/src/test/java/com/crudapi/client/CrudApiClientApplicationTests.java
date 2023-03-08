7b716663277cb452a8c1cd7f2b1ed5ee0dafcc90
Jhonatas <jhonatasoliveira021@gmail.com>
2023-03-08 15:27:34 -0300

    feat: first commit

---
+++ back_end/src/test/java/com/crudapi/client/CrudApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapi.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
